2178e9dfa3688b8fd373200fef8c43009bba0111
bangnokia <bangnokia@gmail.com>
2021-07-19 14:36:41 +0700

    change output from base64 to json encoded string

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ $clockwerk = new BangNokia\Psycho\Clockwerk();
 $output = $clockwerk->bootstrapAt($arguments['target'])->execute(base64_decode(trim($arguments['code'])));
 
 $writer = new \Symfony\Component\Console\Output\ConsoleOutput();
-$writer->writeln(base64_encode($output));
+$writer->writeln(json_encode([
+    'output' => $output,
+    'meta' => []
+]));
 
 return 0;
 
