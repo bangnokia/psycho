4fc4d7c10fef074900902a4ab1d41d0028e2cd81
bangnokia <bangnokia@gmail.com>
2021-07-22 04:06:24 +0700

    wtf is this

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 
+use Symfony\Component\Console\Output\ConsoleOutput;
+
 include __DIR__.'/vendor/autoload.php';
 
-define('PSYCHO_VERSION', '0.1.0');
+const PSYCHO_VERSION = '0.1.0';
 
 $arguments = getopt('', ['target:', 'code:']);
 
 $clockwerk = new BangNokia\Psycho\Clockwerk();
 
-$output = $clockwerk->bootstrapAt($arguments['target'])->execute(base64_decode(trim($arguments['code'])));
+$output = $clockwerk->bootstrapAt($arguments['target'] ?? '')->execute(base64_decode(trim($arguments['code'])));
 
-$writer = new \Symfony\Component\Console\Output\ConsoleOutput();
+$writer = new ConsoleOutput();
 $writer->writeln(base64_encode($output));
 
 return 0;
